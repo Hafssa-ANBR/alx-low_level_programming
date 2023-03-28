fec2c7b994880d14949d9c7c1638e9ddc8d96882
hafssa anebarou <hafssa.anebarou@gmail.com>
2023-03-28 16:10:20 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd>
+
+/**
+ *_putchar - write the character h to stdout
+ *@h: The character to print
+ *
+ *Return: On success 1.
+ *on error, -1 is returned, and error is set appropriately
+ * */
+int _putchar(char h)
+{
+	return (write(1,&h,1));
+}
+
